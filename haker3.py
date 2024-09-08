ad76e0f85b8180e2cd171833c5ac7202de3eb49d
Влад <banshchikovvl@yandex.ru>
2024-09-08 23:36:39 +0700

    Initial commit

---
+++ haker3.py
@@ -0,0 +1,23 @@
+# вводное задание Типы данных
+print("Hello world")
+print(5-5)
+print(5%2)
+print(type(5.0+5))
+print(type(5>6))
+print(5.0==5,type('Hello World!'),',Hello + World,','Hello '+'World '+'=)')
+print(15<=10 and 10==10)
+
+# Индексация строк
+x = "Vludislav"
+y = 'Yana'
+z = 'banchikov'
+print(x[:],'+',y[:],'=',x[1:3]+z[:1]+z[7:] )
+name = 'Chris'
+food = 'creme brulee'
+print(f'Hello. My name is {name} and I like {food}.')
+print('the happiest person in the whole wide world.'.index('the',10,44))
+sentence = 'The Cat in the Hat'
+print(sentence.upper()) #=> 'THE CAT IN THE HAT'
+print(sentence.lower()) #=> 'the cat in the hat'
+
+
